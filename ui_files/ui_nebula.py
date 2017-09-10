ecafc2113e5828e4603ffcee2c0ed0e669438da9
atillaozbiyik <atillaozbiyik@hotmail.com>
2017-09-10 14:34:24 +0300

    added buttons

--- ui_files/ui_nebula.py
+++ ui_files/ui_nebula.py
@@ -46,10 +46,94 @@ class Ui_MainWindow(object):
         self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
         self.mainLayout.addWidget(self.groupBox, 1, 0, 1, 1)
         self.groupBox_5 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_5.setMinimumSize(QtCore.QSize(0, 30))
-        self.groupBox_5.setMaximumSize(QtCore.QSize(16777215, 30))
+        self.groupBox_5.setMinimumSize(QtCore.QSize(0, 50))
+        self.groupBox_5.setMaximumSize(QtCore.QSize(16777215, 50))
+        self.groupBox_5.setStyleSheet("QPushButton:hover{\n"
+"       color:rgb(224,224,224);\n"
+"        background:rgb(24,39,45);\n"
+"}\n"
+"QPushButton {\n"
+"        border:1px solid rgb(64,75,80);\n"
+"        border-radius: 2px;\n"
+"        padding: 5px 5px 2px 5px;\n"
+"        background:rgb(44,59,65);\n"
+"        color: rgb(240,240,240);\n"
+"        height: 30px;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"        background: rgb();\n"
+"        padding-top: 2px;\n"
+"        padding-left: 4px;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"        background: rgb();\n"
+"        padding-top: 2px;\n"
+"        padding-left: 4px;\n"
+"}\n"
+"QPushButton:disabled{\n"
+"        background: rgb(34,45,50);\n"
+"        padding-top: 2px;\n"
+"        padding-left: 4px;\n"
+"        color: rgb(100,100,100);\n"
+"}")
         self.groupBox_5.setTitle("")
         self.groupBox_5.setObjectName("groupBox_5")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.groupBox_5)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.groupBox_5)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setMaximumSize(QtCore.QSize(100, 16777215))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/images/resources/run.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.pushButton_2 = QtWidgets.QPushButton(self.groupBox_5)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy)
+        self.pushButton_2.setMaximumSize(QtCore.QSize(100, 16777215))
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(":/images/resources/clean.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_2.setIcon(icon1)
+        self.pushButton_2.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.pushButton_4 = QtWidgets.QPushButton(self.groupBox_5)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_4.sizePolicy().hasHeightForWidth())
+        self.pushButton_4.setSizePolicy(sizePolicy)
+        self.pushButton_4.setMaximumSize(QtCore.QSize(100, 16777215))
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(":/images/resources/install.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_4.setIcon(icon2)
+        self.pushButton_4.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.horizontalLayout.addWidget(self.pushButton_4)
+        self.pushButton_3 = QtWidgets.QPushButton(self.groupBox_5)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_3.sizePolicy().hasHeightForWidth())
+        self.pushButton_3.setSizePolicy(sizePolicy)
+        self.pushButton_3.setMaximumSize(QtCore.QSize(100, 16777215))
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap(":/images/resources/debug.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_3.setIcon(icon3)
+        self.pushButton_3.setIconSize(QtCore.QSize(32, 32))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout.addWidget(self.pushButton_3)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
         self.mainLayout.addWidget(self.groupBox_5, 0, 0, 1, 3)
         self.groupBox_3 = QtWidgets.QGroupBox(self.centralwidget)
         self.groupBox_3.setTitle("")
@@ -113,6 +197,10 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "Project:"))
+        self.pushButton.setText(_translate("MainWindow", "Run"))
+        self.pushButton_2.setText(_translate("MainWindow", "Clean"))
+        self.pushButton_4.setText(_translate("MainWindow", "Install"))
+        self.pushButton_3.setText(_translate("MainWindow", "Debug"))
         self.menuFile.setTitle(_translate("MainWindow", "File"))
         self.menuEdit.setTitle(_translate("MainWindow", "Edit"))
         self.menuView.setTitle(_translate("MainWindow", "View"))
@@ -120,3 +208,4 @@ class Ui_MainWindow(object):
         self.menuTools.setTitle(_translate("MainWindow", "Tools"))
         self.menuHelp.setTitle(_translate("MainWindow", "Help"))
 
+import rsrcs_rc
