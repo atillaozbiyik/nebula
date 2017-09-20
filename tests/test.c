f624142d7dc5c8d2b73ecba9dc5f5c6471c59918
mehmetb <mehmetb@desird.com>
2017-09-20 10:52:55 +0300

    AST parser and Outline view is added. (Not completed yet)

---
+++ tests/test.c
@@ -0,0 +1,106 @@
+//#include <stdio.h>
+
+int dummy_var;
+#define dummy_define;
+
+#ifdef dummy_define:
+    printf("Dummy define defined");
+#else
+    printf("Dummy define defined");
+#endif
+
+// This is single line comment
+/* This is 
+multiline
+comment
+Block
+*/
+
+union
+{
+    unsigned int All;
+    struct
+    {
+        unsigned char Byte1;
+        unsigned char Byte2;
+        unsigned char Byte3;
+        unsigned char Byte4;
+    } structsample;
+} ui32;
+
+int main()
+{
+    int i, space, rows, k=0, count = 0, count1 = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+
+    for(i=1; i<=rows; ++i)
+    {
+        for(space=1; space <= rows-i; ++space)
+        {
+            printf("  ");
+            ++count;
+        }
+
+        while(k != 2*i-1)
+        {
+            if (count <= rows-1)
+            {
+                printf("%d ", i+k);
+                ++count;
+            }
+            else
+            {
+                ++count1;
+                printf("%d ", (i+k-2*count1));
+            }
+            ++k;
+        }
+        count1 = count = k = 0;
+
+        printf("\n");
+    }
+    return 0;
+}
+
+void main2()
+{
+    int i, space, rows, k=0, count = 0, count1 = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+
+    for(i=1; i<=rows; ++i)
+    {
+        for(space=1; space <= rows-i; ++space)
+        {
+            printf("  ");
+            ++count;
+        }
+
+        while(k != 2*i-1)
+        {
+            if (count <= rows-1)
+            {
+                printf("%d ", i+k);
+                ++count;
+            }
+            else
+            {
+                ++count1;
+                printf("%d ", (i+k-2*count1));
+            }
+            ++k;
+        }
+        count1 = count = k = 0;
+
+        printf("\n");
+    }
+}
+
+//Dummy Block
+{
+
+
+}
---
+++ tools.py
@@ -0,0 +1,5 @@
+'''
+This function may be improved later, i.e. write to txt file, database, GUI etc...
+'''
+def debug(txt):
+    print("DEBUG :" + txt)
